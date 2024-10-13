58776843668ce53467d13aaa6864bb38bea097bc
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-10-13 19:53:32 +0700

    Data Class

---
+++ app/src/main/java/com/example/myapp/DataClass.kt
@@ -0,0 +1,27 @@
+package com.example.myapp
+
+//Data Class adalah yang digunakan untuk menyimpan data
+//Data Class menyediakan fungsi untuk meng-override fungsi equals(), hashcode(), dan toString()
+//Data Class juga menyediakan fungsi copy() untuk membuat salinan objek dengan beberapa perubahan.
+
+data class DataClass(
+    val id: Int,
+    val email: String
+)
+
+fun main() {
+    val data = DataClass(1,"indra@gmail.com")
+    println(data)
+
+    //Fungsi equals
+    val data2 = DataClass(1,"makan@gmail.com")
+    println(data==data2) //false
+
+    //Fungsi Copy
+    val data3 = data.copy()
+    println(data3)
+
+    //Fungsi Copy dan Perubahan
+    val data4 = data.copy(email = "sahar@gmail.com")
+    println(data4)
+}
