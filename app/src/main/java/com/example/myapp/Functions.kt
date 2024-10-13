972155dc18d6d512e909118331e8da49fa853b27
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-10-13 19:44:50 +0700

    funmain Functions

--- app/src/main/java/com/example/myapp/Functions.kt
+++ app/src/main/java/com/example/myapp/Functions.kt
@@ -25,3 +25,15 @@ fun withDefaultParameter(name: String= " Indra", age: Int) {
     println("Hello,$name! You are $age years old.")
 }
 
+fun withReturn(panjang: Int, lebar: Int) {
+    return println(panjang * lebar)
+}
+
+fun main() {
+    withoutParameter()
+    withParameter("John")
+
+    withNamedArgument(name = "John", age = 30)
+    withDefaultParameter(age = 25)
+}
+
