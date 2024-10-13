f527eaac5327c30ad265f955024abf11c4ba5d1b
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-10-13 10:37:21 +0700

    List Mutable

--- app/src/main/java/com/example/myapp/Collection.kt
+++ app/src/main/java/com/example/myapp/Collection.kt
@@ -7,4 +7,24 @@ fun ContohList() {
     //List Read Only
     val readOnlyAbjad = listOf("A", "B", "C")
     println(readOnlyAbjad)
+
+    //List Mutable
+    val mutableShapes: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
+    println(mutableShapes)
+
+    //Menambahkan data kedalam List Mutable
+    mutableShapes.add("Circle")
+    println(mutableShapes)
+
+    //Menghapus data dari List Mutable
+    mutableShapes.remove("Triangle")
+    println(mutableShapes)
+
+    //Mengubah data didalam List Mutable
+    mutableShapes[0] = "Oval"
+    println(mutableShapes)
+
+    //List Read-Only
+    val shapeLocked: List<String> = mutableShapes
+    println(shapeLocked)
 }
