cfec873434f9ce1813fe25200145584ed7c8149d
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-10-13 09:54:58 +0700

    List Read Only

---
+++ app/src/main/java/com/example/myapp/Collection.kt
@@ -0,0 +1,10 @@
+package com.example.myapp
+
+//List adalah kumpulan data yang disusun secara berurutan dari
+
+fun ContohList() {
+    println("=== List ===")
+    //List Read Only
+    val readOnlyAbjad = listOf("A", "B", "C")
+    println(readOnlyAbjad)
+}
