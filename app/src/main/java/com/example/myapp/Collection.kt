2cc39fc0b739c2b39b180f6ec305c246f8f6b85f
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-10-13 11:10:05 +0700

    Fun Main Collection

--- app/src/main/java/com/example/myapp/Collection.kt
+++ app/src/main/java/com/example/myapp/Collection.kt
@@ -28,3 +28,59 @@ fun ContohList() {
     val shapeLocked: List<String> = mutableShapes
     println(shapeLocked)
 }
+
+fun ContohSet(){
+    println()
+    println("=== Set ===")
+
+    //Set Read-Only
+    val readOnlyAbjad = setOf("A", "B", "C")
+    println(readOnlyAbjad)
+
+    //Set Mutable
+    val mutableShapes: MutableSet<String> = mutableSetOf("Circle", "Square", "Triangle")
+    println(mutableShapes)
+
+    //Menambahkan data kedalam Set Mutable
+    mutableShapes.add("Rectangle")
+    println(mutableShapes)
+
+    //Set Read-Only
+    val shapeLocked: Set<String> = mutableShapes
+    println(shapeLocked)
+}
+
+fun ContopMap() {
+    println()
+    println("=== Map ===")
+
+    //Map Read-Only
+    val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(readOnlyShape)
+
+    //Map Mutable
+    val mutableShapes: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(mutableShapes)
+
+    //Menambahkan data kedalam Map Mutable
+    mutableShapes["Rectangle"] = 4
+    println(mutableShapes)
+
+    //Menghapus data dari Map Mutable
+    mutableShapes.remove("Circle")
+    println(mutableShapes)
+
+    //Mengubah data didalam Map Mutable
+    mutableShapes["Square"] = 5
+    println(mutableShapes)
+
+    //Map Read-Only
+    val shapesLocked: Map<String, Int> = mutableShapes
+    println(shapesLocked)
+}
+
+fun main(){
+    ContohList()
+    ContohSet()
+    ContopMap()
+}
