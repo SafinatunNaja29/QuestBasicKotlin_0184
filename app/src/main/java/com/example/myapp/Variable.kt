3b918d6528797aa64bd0846b67ad7c0bf8147840
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-10-13 11:19:29 +0700

    new

---
+++ app/src/main/java/com/example/myapp/Variable.kt
@@ -0,0 +1,34 @@
+package com.example.myapp
+
+fun main(){
+    //Byte
+    val ByteValue : Byte = 10
+    println("Byte: $ByteValue")
+
+    // Int
+    val intValue: Int = 30
+    println(intValue)
+
+    // Float
+    val floatValue : Float = 50.6f
+    println("Float: $floatValue")
+
+    // Double
+    val doubleValue : Double = 60.6
+    println("Double: $doubleValue")
+
+    // Char
+    val charValue : Char = 'A'
+    println("Char: $charValue")
+
+    // Boolean
+    val booleanValue : Boolean = true
+    println("Boolean: $booleanValue")
+
+    // String
+    var stringValue : String = "Hello, World"
+    println("String: $stringValue")
+
+    stringValue = "Hello, Kotlin"
+    println("String: $stringValue")
+}
